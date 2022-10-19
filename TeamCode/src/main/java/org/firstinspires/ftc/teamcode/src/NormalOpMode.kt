8cba5ac482ec1b7921eb9ecc21f0b01739bd0b1a
Gilbert O <giloe.dev@gmail.com>
2022-10-19 17:23:50 -0600

    Github you ok???
    It added random lines to this code for some reason

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/NormalOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/NormalOpMode.kt
@@ -10,23 +10,7 @@ import org.firstinspires.ftc.teamcode.src.models.ABot.TeleInstance
 
 @TeleOp(name = "NormalTeleOp", group = "TeleOp Mode")
 class NormalOpMode : LinearOpMode() {
-<<<<<<< Updated upstream
 //    private var bot: AutoInstance? = null
-
-
-    override fun runOpMode() {
-//        bot = AutoInstance(this, hardwareMap, telemetry)
-//        right?.setPower(gamepad1.right_stick_y.toDouble());
-//        left?.setPower((-gamepad1.left_stick_y).toDouble());
-        waitForStart()
-        while(opModeIsActive()){
-        telemetry.addData("GamePad 1X %f", gamepad1.left_stick_x)
-        telemetry.addData("GamePad 1Y %f", gamepad1.left_stick_y)
-        telemetry.update()
-        }
-=======
-//    private var bot: TeleInstance? = null
-
     override fun runOpMode() {
 //        bot = TeleInstance(this, hardwareMap, telemetry)
         telemetry.addData(">", "Ready")
@@ -38,7 +22,5 @@ class NormalOpMode : LinearOpMode() {
             telemetry.addData("GamePad 1Y", gamepad1.left_stick_y)
             telemetry.update()
         }
-
->>>>>>> Stashed changes
     }
 }
